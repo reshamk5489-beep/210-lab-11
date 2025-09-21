5fa7cbb897f3418519014d7e15890f11d1b55bfc
Resham <resha@Mac.attlocal.net>
2025-09-21 15:14:41 -0700

    Input Department Information

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 using namespace std;
 
+// Comment #1: Define a constant for the maximum number of departments.
 const int MAX_DEPARTMENTS = 4;
 
+// Comment #2: Define a constant for the maximum number of employees per department.
+const int MAX_EMPLOYEES = 3;  
+
+// Comment #3: Define a structure to represent a department containing its 
+// name, ID, and a dynamic array of employee IDs.
 struct Department
 {
     string name;
     int id;
     int *employeeIds;
 
+    // Comment #4: Destructor to free dynamically allocated memory for employee IDs.
     ~Department()
     {
         if (employeeIds)
@@ -24,9 +31,29 @@ void inputDepartment(Department *deptPtr);
 
 int main()
 {
+    // Comment #5: Dynamically allocate an array of Department structures.
     Department *departments = new Department[MAX_DEPARTMENTS];
+
+    // Comment #6: Input details for each department.
     for (int i = 0; i < MAX_DEPARTMENTS; ++i)
     {
-       
+       inputDepartment(&departments[i]);
+    }
+}
+
+void inputDepartment(Department *deptPtr)
+{
+    cout << "Enter department name: ";
+    cin >> deptPtr->name;
+
+    cout << "Enter the department ID:";
+    cin >> deptPtr->id;
+
+    deptPtr->employeeIds = new int[MAX_EMPLOYEES];
+
+    for(int i = 0; i < MAX_EMPLOYEES; ++i)
+    {
+        cout << "Enter employee ID " << (i + 1) << ": ";
+        cin >> deptPtr->employeeIds[i];
     }
 }
